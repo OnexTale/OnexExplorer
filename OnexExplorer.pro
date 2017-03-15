765f84235af319c5d550b5a5f6f709ffe0cf600b
morsisko <morsisko@gmail.com>
2017-03-15 15:13:35 +0100

    simple decrypt interface

--- OnexExplorer.pro
+++ OnexExplorer.pro
@@ -24,8 +24,10 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    inosdecryptor.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+    inosdecryptor.h
 
 FORMS    += mainwindow.ui
---
+++ inosdecryptor.h
@@ -0,0 +1,13 @@
+#ifndef INOSDECRYPTOR_H
+#define INOSDECRYPTOR_H
+#include <QByteArray>
+
+class INosDecryptor
+{
+public:
+    virtual QByteArray encrypt(QByteArray& array) = 0;
+    virtual QByteArray decrypt(QByteArray& array) = 0;
+    virtual INosDecryptor() = 0;
+};
+
+#endif // INOSDECRYPTOR_H

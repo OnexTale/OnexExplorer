3b6964577883169856f2d1b4bdf12b55112764a2
friends111 <idylosee@gmail.com>
2017-04-15 16:53:54 +0200

    remove unnecesaary stuffs

--- Source/ImageConverter.h
+++ Source/ImageConverter.h
@@ -18,21 +18,6 @@ public:
         BGRA8888,
         NSTC
     };
-    enum GridColors
-    {
-        GRAY = qRgb(100, 100, 100),
-        BLACK = qRgb(0, 0, 0),
-        WHITE = qRgb(255, 255, 255),
-        RED = qRgb(150, 100, 100),
-        GREEN = qRgb(100, 150, 100),
-        BLUE = qRgb(100, 100, 150),
-        YELLOW = qRgb(150, 150, 100),
-        PURPLE = qRgb(150, 100, 150),
-        H_GREEN = qRgb(0, 200, 50),
-        H_BLUE = qRgb(0, 50, 200),
-        H_RED = qRgb(200, 50, 0),
-        H_YELLOW = qRgb(250, 230, 10)
-    };
     ImageConverter();
     QImage getFromData(QByteArray& array, int width, int height, ImageTypes format);
 };

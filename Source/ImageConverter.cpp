b3289b854dd4202cf0486b888427663e2d311b7c
morsisko <morsisko@gmail.com>
2017-07-18 23:59:59 +0200

    images are displayed correctly

--- Source/ImageConverter.cpp
+++ Source/ImageConverter.cpp
@@ -28,14 +28,16 @@ QImage ImageConverter::convertGBAR4444(QByteArray &array, int width, int height,
 
 QImage ImageConverter::convertBGRA8888(QByteArray &array, int width, int height, int startByte)
 {
-    QImage img((uchar*)array.mid(startByte).data(), width, height, QImage::Format_RGBA8888);
+    QByteArray tempArray = array.mid(startByte);
+    QImage img((uchar*)tempArray.data(), width, height, QImage::Format_RGBA8888);
 
     return img.rgbSwapped();
 }
 
 QImage ImageConverter::convertARGB555(QByteArray& array, int width, int height, int startByte)
 {
-    QImage img((uchar*)array.mid(startByte).data(), width, height, QImage::Format_RGB555);
+    QByteArray tempArray = array.mid(startByte);
+    QImage img((uchar*)tempArray.data(), width, height, QImage::Format_RGB555);
 
     return img;
 }

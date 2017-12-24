af44777f6455a1e02b6d9b2758d722930f32e8f1
morsisko <morsisko@gmail.com>
2017-12-24 13:46:17 +0100

    text encrypt

--- Source/Ui/OnexTreeText.cpp
+++ Source/Ui/OnexTreeText.cpp
@@ -1,8 +1,20 @@
 #include "OnexTreeText.h"
 #include "SingleTextFilePreview.h"
+#include "../Openers/NosTextOpener.h"
 
-OnexTreeText::OnexTreeText(QString name, int fileNumber, int isDat, QByteArray content) : OnexTreeItem(name, content)
+int OnexTreeText::getFileNmber() const
 {
+    return fileNmber;
+}
+
+int OnexTreeText::getIsDat() const
+{
+    return isDat;
+}
+
+OnexTreeText::OnexTreeText(QString name, NosTextOpener *opener, int fileNumber, int isDat, QByteArray content) : OnexTreeItem(name, content)
+{
+    this->opener = opener;
     this->fileNmber = fileNumber;
     this->isDat = isDat;
 }
@@ -13,6 +25,32 @@ QWidget *OnexTreeText::onClicked()
     window->setWindowTitle(this->getName());
 
     return window;
+}
+
+void OnexTreeText::onExporAsOriginal()
+{
+    QString fileName = getSaveDirectory(this->getName(), "NOS Archive (*.NOS)");
+
+    if (fileName.isEmpty())
+        return;
+
+    if (!fileName.endsWith(".NOS"))
+        fileName += ".NOS";
+
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly))
+    {
+        QMessageBox::critical(NULL, "Woops", "Couldn't open this file for writing");
+        return;
+    }
+
+    if (file.write(opener->encrypt(this)) == -1)
+    {
+        QMessageBox::critical(NULL, "Woops", "Couldn't write to this file");
+        return;
+    }
+
+    QMessageBox::information(NULL, "Yeah", "File exported into .NOS");
 }
 
 OnexTreeText::~OnexTreeText()
